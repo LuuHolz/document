23b2e6f6d3353d0869c7afba6f984dfbc35dfff3
Luciana <luciana.holzmann@gmail.com>
2024-05-22 00:41:37 +0200

    [FEAT] Textos e iconos de Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import Pets from '../assets/mascotas.png';
+import Document from '../assets/identificacionpng.png';
+import AdoptoParaMi from '../assets/dueno-de-una-mascota (1).png';
+import AdoptoParaOtro from '../assets/dueno-de-una-mascota.png'
+import Cerrar from '../assets/cerrar.png';
+import Atras from '../assets/atras.png'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homeContainer'>
+        <div>
+            <img src={Atras} alt="" />
+            <img src={Pets} alt="pet" className='principalIcon'/>
+            <img src={Cerrar} alt="cerrar" className='iconCerrar'/>
+        </div>
+        <div>
+            Documento del responsable de la adopcion
+        </div>
+        <div>
+            DEBES AÑADIR UN DOCUMENTO
+        </div>
+        <img src={Document} alt="document" className='document'/>
+        <div>
+            <p>
+                Para iniciar la pre-adopcion, es necesario tener registrado un documento de identificacion.
+            </p>
+        </div>
+        <div>
+            <p>Titutlar del documento</p>
+        </div>
+        
+
+    </div>
   )
 }
 
